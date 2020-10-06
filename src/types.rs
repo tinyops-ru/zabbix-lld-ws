b00d43ec1ed2d559a28c0d438c186f76d7491181
Eugene Lebedev <duke.tougu@gmail.com>
2020-10-06 16:34:52 +0300

    [+] type alias: operation result

--- src/types.rs
+++ src/types.rs
@@ -1,5 +1,7 @@
 pub mod types {
     use crate::errors::errors::OperationError;
 
+    pub type OperationResult<R> = Result<R, OperationError>;
+
     pub type StringResult = Result<String, OperationError>;
 }
