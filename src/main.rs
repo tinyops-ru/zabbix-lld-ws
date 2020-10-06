93e3e7922a6be0e421ad3b2320276c400ef7f628
Eugene Lebedev <duke.tougu@gmail.com>
2020-10-06 17:48:47 +0300

    [+] feature: set logging level with option '--log-level'

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ extern crate log4rs;
 use std::path::Path;
 use std::process::exit;
 
+use clap::{App, Arg};
 use regex::Regex;
 use reqwest::blocking::Client;
 
@@ -34,12 +35,31 @@ mod logging;
 mod errors;
 mod http;
 
+const LOG_LEVEL_ARGUMENT: &str = "log-level";
 const LOG_LEVEL_DEFAULT_VALUE: &str = "info";
 
 const ERROR_EXIT_CODE: i32 = 1;
 
 fn main() {
-    let logging_config = get_logging_config(LOG_LEVEL_DEFAULT_VALUE);
+    let matches = App::new("WSZL tool")
+        .version("0.3.0")
+        .author("Eugene Lebedev <duke.tougu@gmail.com>")
+        .about("Add Web scenarios support for Zabbix Low Level Discovery")
+        .arg(
+            Arg::with_name(LOG_LEVEL_ARGUMENT)
+                .help("set logging level. possible values: debug, info, error, warn, trace")
+                .long(LOG_LEVEL_ARGUMENT)
+                .case_insensitive(true)
+                .takes_value(true).required(false)
+                .default_value(LOG_LEVEL_DEFAULT_VALUE)
+        )
+        .get_matches();
+
+    let logging_level: &str = if matches.is_present(LOG_LEVEL_ARGUMENT) {
+        matches.value_of(LOG_LEVEL_ARGUMENT).unwrap()
+    } else { LOG_LEVEL_DEFAULT_VALUE };
+
+    let logging_config = get_logging_config(logging_level);
     log4rs::init_config(logging_config).unwrap();
 
     let config_file_path = Path::new("wszl.yml");
