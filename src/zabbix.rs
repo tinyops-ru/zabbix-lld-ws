0935be2805e04f7d664ea09edf3ed6bce52811b6
Eugene Lebedev <duke.tougu@gmail.com>
2020-10-06 16:10:13 +0300

    [+] common zabbix request struct

--- src/zabbix.rs
+++ src/zabbix.rs
@@ -1,6 +1,17 @@
 pub mod zabbix {
+    use serde::Serialize;
+
     pub const JSONRPC: &str = "2.0";
 
     pub const CONTENT_TYPE_HEADER: &str = "Content-Type";
     pub const CONTENT_TYPE_JSON: &str = "application/json";
+
+    #[derive(Serialize)]
+    pub struct ZabbixRequest<P: Serialize> {
+        pub jsonrpc: String,
+        pub method: String,
+        pub params: P,
+        pub auth: String,
+        pub id: i8
+    }
 }
