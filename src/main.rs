b3c2cca89106d13d7f63b0d81d68ccbbea629b6e
Eugene Lebedev <duke.tougu@gmail.com>
2020-12-01 10:48:19 +0300

    [~] bump app version to 0.5.0

--- src/main.rs
+++ src/main.rs
@@ -49,7 +49,7 @@ const ERROR_EXIT_CODE: i32 = 1;
 
 fn main() {
     let matches = App::new("WSZL tool")
-        .version("0.4.0")
+        .version("0.5.0")
         .author("Eugene Lebedev <duke.tougu@gmail.com>")
         .about("Add Web scenarios support for Zabbix Low Level Discovery")
         .arg(
