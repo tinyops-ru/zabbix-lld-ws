4716e8437df763fee17b040abb1df22d3e56179d
Eugene Lebedev <duke.tougu@gmail.com>
2024-01-26 18:39:05 +0300

    Remove deprecated code

--- src/zabbix/items.rs
+++
@@ -1,8 +0,0 @@
-use serde_derive::Deserialize;
-
-#[derive(Deserialize, Debug)]
-pub struct ZabbixItem {
-    pub name: String,
-    pub key_: String,
-    pub hostid: String
-}
--- src/zabbix/triggers/find.rs
+++
@@ -1,8 +0,0 @@
-use serde_derive::Deserialize;
-
-use crate::zabbix::triggers::ZabbixTrigger;
-
-#[derive(Deserialize)]
-pub struct TriggerSearchResponse {
-    pub result: Vec<ZabbixTrigger>
-}
